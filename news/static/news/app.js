4270451625818d5b1eee2f0368d6979ce3fb8676
zzhalex <zzh001xx@hotmail.com>
2020-07-09 01:16:03 -0700

    add js nav

--- news/static/news/app.js
+++ news/static/news/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+	navSwitch();
 });
+
+function navSwitch(){
+    var pathname = window.location.pathname;
+    if(pathname=='/'){
+        cleanNavActive();
+        $($(".nav-link")[0]).addClass("active");
+    }else if(pathname=='/addnew/'){
+        cleanNavActive();
+        $($(".nav-link")[1]).addClass("active");
+    }
+}
+
+function cleanNavActive(){
+    $(".nav-link").removeClass("active");
+}
