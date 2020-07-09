d6beaf136dffdbe53ba9820a9307c7c717ad4093
zzhalex <zzh001xx@hotmail.com>
2020-07-09 00:53:19 -0700

    add css and js

--- news/views.py
+++ news/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return render(request,'news/index.html')
+	return render(request,'news/home.html')
 
 def addnew(request):
 	return render(request,'news/add.html',{'addnew':True})
